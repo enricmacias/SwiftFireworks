58c1cd50107dc54053b5a3cfefc597a9f76f3707
Enric Macias Lopez <enric_maciaslopez@cyberagent.co.jp>
2017-07-31 12:21:43 +0900

    first commit

---
+++ Example/SwiftFireworks/SkyViewController.swift
@@ -0,0 +1,67 @@
+//
+//  ViewController.swift
+//  Fireworks
+//
+//  Created by enric_maciaslopez on 07/28/2017.
+//  Copyright (c) 2017 enric_maciaslopez. All rights reserved.
+//
+
+import UIKit
+import Fireworks
+
+class SkyViewController: UIViewController {
+
+    //------------------------------------------------------------------------------
+    // MARK: - Properties -
+    //------------------------------------------------------------------------------
+    
+    // MARK: - IBOutlets
+    @IBOutlet var fireworkButton: UIButton!
+    @IBOutlet var fireworkSetButton: UIButton!
+    
+    
+    //------------------------------------------------------------------------------
+    // MARK: - Lifecycle -
+    //------------------------------------------------------------------------------
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = UIColor.black
+    }
+    
+    //------------------------------------------------------------------------------
+    // MARK: - Actions -
+    //------------------------------------------------------------------------------
+    
+    @IBAction func didPressFirework() {
+        let screenBounds = UIScreen.main.bounds
+        
+        let leftBounds: CGFloat = screenBounds.origin.x + 20
+        let rightBounds: CGFloat = screenBounds.origin.x + screenBounds.size.width - 20.0
+        let topBounds: CGFloat = screenBounds.origin.y + 20.0
+        let bottomBounds: CGFloat = screenBounds.origin.y + screenBounds.size.height - 20.0
+        let posX: CGFloat = Fireworks.sharedInstance.randomCGFloat(min: leftBounds, max: rightBounds)
+        let posY: CGFloat = Fireworks.sharedInstance.randomCGFloat(min: topBounds, max: bottomBounds)
+        
+        Fireworks.sharedInstance.showFirework(inView: view,
+                                              andPosition: CGPoint(x: posX, y: posY))
+    }
+    
+    @IBAction func didPressFireworkSetButton() {
+        let screenBounds = UIScreen.main.bounds
+        
+        let leftBounds: CGFloat = screenBounds.origin.x + 20
+        let rightBounds: CGFloat = screenBounds.origin.x + screenBounds.size.width - 20.0
+        let topBounds: CGFloat = screenBounds.origin.y + 20.0
+        let bottomBounds: CGFloat = screenBounds.origin.y + screenBounds.size.height - 20.0
+        let posX = Fireworks.sharedInstance.randomCGFloat(min: leftBounds, max: rightBounds)
+        let posY = Fireworks.sharedInstance.randomCGFloat(min: topBounds, max: bottomBounds)
+        
+        Fireworks.sharedInstance.showFireworkSet(inView: view,
+                                                 andPosition: CGPoint(x: posX, y: posY),
+                                                 numberOfFireworks: 6)
+    }
+
+}
+
